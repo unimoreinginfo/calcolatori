bb70294f234829ac6a08109b9d56b580bbe92878
Carmine Zaccagnino <carmine@carminezacc.com>
2020-10-20 15:55:58 +0200

    tolte cose inutili da es 8

--- esercizio-8/main.c
+++ esercizio-8/main.c
@@ -3,43 +3,27 @@
 int main(void) {
 	char str[255] = "Ingegneria Informatica fa schifo ma si diventa ricchi";
 
+	char *p = str;
+
 	__asm {
 	// rendere maiuscole le consonanti minuscole
 	mov esi, 0
+	mov eax, p
 
 	ciclo:
 	mov bl, byte ptr [eax+esi]
-	cmp bl, 'A'
-	jl fineciclo
+	cmp bl, 'a'
+	jng fineciclo
 	cmp bl, 'z'
 	jg fineciclo
-	cmp bl, 'Z'
-	jng confronti
-	cmp bl, 'a'
-	jl fineciclo
-	confronti:
-	cmp bl, 'a'
-	je fineciclo
-	cmp bl, 'A'
-	je fineciclo
 	cmp bl, 'e'
-	je fineciclo
-	cmp bl, 'E'
 	je fineciclo
 	cmp bl, 'i'
 	je fineciclo
-	cmp bl, 'I'
-	je fineciclo
 	cmp bl, 'o'
-	je fineciclo
-	cmp bl, 'O'
 	je fineciclo
 	cmp bl, 'u'
 	je fineciclo
-	cmp bl, 'U'
-	je fineciclo
-	cmp bl, 'a'
-	jl fineciclo
 	sub bl, 20h
 	mov [eax+esi], bl
 	fineciclo:
@@ -50,7 +34,6 @@ int main(void) {
 
 
 	fine:
-	mov num, ecx
 	}
 
 	printf("stringa cambiata: %s", str);
